57c2776d0333a81ab771880b32e82b57bcec89a2
hellena345 <hellena345@naver.com>
2023-12-19 14:18:16 +0900

    프로젝트 코드업로드

---
+++ app/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>customer page 입니다.</div>
+  )
+}
+
+export default Page
---
+++ app/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>invoice 입니다</div>
+  )
+}
+
+export default Page
